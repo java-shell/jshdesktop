a98792e81211350eb97658c02dd72294c125ff00
Daniel Schirripa <danschirripa98@gmail.com>
2022-04-28 14:30:34 -0400

    Fixed InitCompletiongEvent missing annotation

--- src/jshdesktop/events/InitCompletionEvent.java
+++ src/jshdesktop/events/InitCompletionEvent.java
@@ -4,6 +4,7 @@ import terra.shell.utils.system.GeneralEvent;
 
 public class InitCompletionEvent extends GeneralEvent{
 
+	@EventPriority(id=GENERAL_TYPE, value=5)
 	public InitCompletionEvent(Object[] args) {
 		super("jshdesktop_initcompletion", args);
 	}
